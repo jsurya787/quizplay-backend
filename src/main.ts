128dddacf69c80d5c025a88bf358bb1d36474b5c
jaisurya <jaisurya@Mac.lan>
2026-01-11 18:54:48 +0530

    .

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,27 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:4200', // Angular app
-    credentials: true,               // if using cookies / auth
+    origin: [
+      'http://localhost:4200',
+      'https://quizplay.co.in',
+      'https://www.quizplay.co.in',
+    ],
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,          // remove extra fields
+      whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true,          // auto-transform payloads
+      transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
+
 bootstrap();
-
-
